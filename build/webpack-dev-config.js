8e324564493988349afa68f3945c8695f2d4986c
illizac <2431770012@qq.com>
2017-09-14 18:29:06 +0800

    complete shopImg upload

--- build/webpack-dev-config.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack')
-var config = require('./webpack-base-config.js')
-
-var webpackDevServer = require('webpack-dev-server')
-
-new webpackDevServer(webpack(config), {
-	contentBase: '../dist/',
-}).listen(8097, 'localhost')
-console.log('server run 8097')
